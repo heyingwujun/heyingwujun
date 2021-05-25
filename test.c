bfcf69406d0981475eb459f00791ee77cf3dc679
heyingwujun <84693122+heyingwujun@users.noreply.github.com>
2021-05-25 05:38:00 -0500

    Add files via upload

---
+++ test.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+
+
+
+
+
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//	//*pc = 'a';
+//	printf("%c\n", *pc);
+//	return 0;
+//}
+//
+
+
+//int main()
+//{
+//	int a = 101;//创建空间a=101
+//	int* p = &a;//找到a的地址=p，地址    就是指针
+//	printf("%p\n", &a);
+//	printf("%p\n", p);
+//	printf("%d\n", *p);//通过地址p找到a，*为解引用操作符
+//	//printf("%p\n", &a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	printf("开始学习\n");
+//	while (line < 2000)
+//	{
+//		printf("敲一行代码%d\n",line);
+//		line++;
+//	}
+//	if (line >= 2000);
+//	printf("好offer\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num = 0;
+//	int arr[] = { 1,2,5,4,3,9,8,7,6,12,13 };
+//	 num = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 1;
+//	printf("%d\n", sizeof a);
+//		return 0;
+//}
